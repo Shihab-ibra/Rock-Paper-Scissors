6e41a751ae76d46efaf1f0e9797e143b3adea40a
Shihab-ibra <138722010+Shihab-ibra@users.noreply.github.com>
2024-01-10 01:20:06 +0200

    rock paper scissors game with a simple UI.

--- script.js
+++ script.js
@@ -6,58 +6,40 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getPlayerChoice() {
-    let playerChoice;
-    do {
-        playerChoice = prompt('Choose Rock, Paper or Scissors? ');
-        if (typeof playerChoice === 'string') playerChoice = playerChoice.toLowerCase();
-    
-    } while ((!gameChoices.includes(playerChoice) && playerChoice) || playerChoice == '')
-    
-    return playerChoice;
-}
-
 function playGame() {
-    let round = 0;
-    let playerChoice;
-    let computerChoice;
     let playerScore = 0;
     let computerScore = 0;
-    let winner = '';
-    for (round; round < 5; round++) {
-        playerChoice = getPlayerChoice();
-        if (playerChoice == null) break;
-        computerChoice = getComputerChoice();
+    const options = document.querySelectorAll(".options");
+    const result = document.querySelector('.result');
+    const pScore = document.querySelector('.playerScore');
+    const cScore = document.querySelector('.computerScore');
+    const resetBtn = document.querySelector('.resetBtn');
 
-        function playRound(playerChoice, computerChoice) {
-            if (playerChoice == computerChoice) {
-                return 'It\'s a tie';
-            } else if (playerChoice == 'rock' && computerChoice == 'scissors' ||
-                    playerChoice == 'paper' && computerChoice == 'rock' ||
-                    playerChoice == 'scissors' && computerChoice == 'paper') {
-                playerScore++;
-                return `You win! ${playerChoice} beats ${computerChoice}`;
-            } else {
-                computerScore++;
-                return `You lose! ${computerChoice} beats ${playerChoice}`;
+    options.forEach((choice) => {
+        choice.addEventListener("click", function () {
+            const playerChoice = this.innerText.toLowerCase();
+            const computerChoice = getComputerChoice();
+
+            const playRound = (playerChoice, computerChoice) => {
+                if (playerChoice == computerChoice) {
+                    result.textContent = 'It\'s a tie';
+                } else if (playerChoice == 'rock' && computerChoice == 'scissors' || 
+                           playerChoice == 'paper' && computerChoice == 'rock' ||
+                           playerChoice == 'scissors' && computerChoice == 'paper') {
+                    pScore.innerText = ++playerScore;
+                    result.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
+                } else {
+                    cScore.innerText = ++computerScore;
+                    result.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
+                }
             }
-        }
-        console.log(playRound(playerChoice, computerChoice));
-    }
-
-    switch (true) {
-        case playerScore == computerScore:
-            winner = 'It\'s a tie';
-            break;
-        case playerScore > computerScore:
-            winner = 'You win the game! Congratulations!';
-            break;
-        default:
-            winner = 'Computer wins the game! Try again next time!';
-            break;
-    }
+            playRound(playerChoice, computerChoice);
+        });
+    });
     
-    let finalScore = `Your score: ${playerScore}\ncomputer score: ${computerScore}\n${winner}`;
-    return finalScore;
+    resetBtn.addEventListener('click', () => {
+        window.location.href = window.location.href;
+    })
 }
-console.log(playGame());
+
+playGame();
